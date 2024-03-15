27d7d5c70c36a21a08423bf047917195cbd566c0
PIP-Bravo <117134989+PIP-Bravo@users.noreply.github.com>
2024-03-16 00:45:02 +0700

    Prak 4 - 24060122130080
    Penerapan inherritance pada tugas 2

---
+++ Tugas prak4/Lecture.java
@@ -0,0 +1,51 @@
+/* Nama : Alfonso Clement Sutantio
+ * NIM : 24060122130080
+ * Praktikum : PBO D1 Praktikum 4
+ */
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Lecture extends Person {
+	
+    // Deklarasi properti dengan enkapsulasi
+    private List<Course> coursesTaught;
+
+    // Konstruktor
+    Lecture(String name, int age, String address, int employeeID) {
+        // Fungsi konstruktor
+        super(name, age, address, employeeID);
+        this.coursesTaught = new ArrayList<>();
+    }
+    
+    // Method lain ( bisa berupa getter maupun setter)
+    public void getDetails() {
+        /* Fungsi untuk print detail dari Lecture,
+           menampilkan nama, umur, alamat, dan NIP
+        */
+    	System.out.println("Nama : "+this.getName());
+    	System.out.println("Umur : "+this.getAge());
+    	System.out.println("Alamat : "+this.getAddress());
+    	System.out.println("NIP : "+this.getID());
+    	System.out.println();
+    }    
+
+    public void teachCourse(Course course) {
+        // Fungsi untuk menambah sebuah course 
+    	this.coursesTaught.add(course);
+    }
+    
+    public void teachCourseDone(Course course) {
+        // Fungsi ini digunakan untuk dosen menghapus course yang telah selesai diampu
+    	this.coursesTaught.remove(course);
+    }
+
+    public void viewCourses() {//Overriding
+        /* Fungsi untuk menampilkan seluruh course yang diampu oleh dosen.
+        */
+    	for(int i = 0; i<=(coursesTaught.size()-1); i+=1) {
+			coursesTaught.get(i).getDetails();
+		}
+    }
+    
+}
